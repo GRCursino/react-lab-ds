7fb71844015693afad63b9a80912e6a10d245eef
GRCursino <guiiix93@gmail.com>
2022-10-20 20:48:09 -0300

    att

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -17,7 +17,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {  
-      config.base = '/react-lab-ds'
+      config.base = '/react-lab-ds/'
     }
 
     return config
